f3c1898c4d95565af7f9e70231a66ab6db4b326e
matheuspinho <matheusluiz0303@outlook.com>
2024-06-13 10:17:37 -0300

    feature-4260

--- src/app/pages/hero/hero.tsx
+++ src/app/pages/hero/hero.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import "./hero.scss"
 import MenuInstitucional from "@/components/ui/header/menuInstitucional";
 import Image from 'next/image'
@@ -9,10 +9,16 @@ import ServiceCard from "@/components/ui/patterns/card/service-card/service-card
 import StateSelector from "@/components/ui/header/stateSelector";
 import imagemClaraPose from '@/assets/img/imagemClaraPose.svg'
 export function Hero():JSX.Element {
+    const [activePlusServices, setActivePlusServices] = useState(false)
     const [listaService, setListaService] = useState([{id:1,title:'Emitir segunda via da conta',icon:'file_copy'},
         {id:2,title:'Informar falta de luz',icon:'flash_off'},{id:3,title:'Solicitar religação',icon:'lightbulb_outline'},{id:4,title:'Passar a conta para seu nome',icon:'supervisor_account'},{id:5,title:'Entender a sua conta de luz',icon:'flash_on'},
         {id:6,title:'Todos os serviços',icon:'grid_view'}
     ])
+
+    const handleClickPlus = ()=> {
+        setActivePlusServices(!activePlusServices)
+
+    }
     return (
         <div className="container">
             <div className="wrapper">
@@ -23,37 +29,45 @@ export function Hero():JSX.Element {
                     </div>
                     <MenuInstitucional/>
                 </div>
-                <div className="body">
-                   <div className="listaItem">
-                       <div className='listaInfo'>
-                           <div>
-                               <div>
-                                   <span>Você está em</span> <StateSelector/>
-                               </div>
-                               <h1>Como podemos te ajudar hoje?</h1>
-                           </div>
-                               <div className='boxYellow'>
-                                   <div className='imagemClara'>
-                                       <Image
-                                           src={imagemClaraPose}
-                                           alt='imagemClaraPose'
-                                           sizes="100vw"
-                                           style={{
-                                               width:'300px',
-                                               height:'128px'
-                                           }}
-                                       />
-                                   </div>
-                                   <p>Prefere resolver pelo WhatsApp? <a>Fale com a Clara</a></p>
-                               </div>
-                       </div>
-                       <div className='listaCardService'>
-                           {listaService.map((item) => (
-                               <ServiceCard key={item.id} title={item.title} symbols={item.icon}/>
-                           ))}
-                       </div>
-                   </div>
+                <div className="body" onClick={handleClickPlus}>
+                    <div className={`listaItem ${activePlusServices ? 'bodyhiddentoTop' : 'bodyActiveBottom'}`}>
+                        <div className='listaInfo'>
+                            <div>
+                                <div>
+                                    <span>Você está em</span> <StateSelector/>
+                                </div>
+                                <h1>Como podemos te ajudar hoje?</h1>
+                            </div>
+                            <div className='boxYellow'>
+                                <div className='imagemClara'>
+                                    <Image
+                                        src={imagemClaraPose}
+                                        alt='imagemClaraPose'
+                                        sizes="100vw"
+                                        style={{
+                                            width: '300px',
+                                            height: '128px'
+                                        }}
+                                    />
+                                </div>
+                                <p>Prefere resolver pelo WhatsApp? <a>Fale com a Clara</a></p>
+                            </div>
+                        </div>
+                        <div className='listaCardService'>
+                            {listaService.map((item) => (
+                                <ServiceCard onClick={handleClickPlus} key={item.id} title={item.title} symbols={item.icon}/>
+                            ))}
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className={`listaServicePlus ${activePlusServices ? 'listaCardServiceActive' : 'listaCardServiceDisable'}`} >
+                <h3>Lista de itens plus</h3>
+                <ul>
+                    <li>Thiago</li>
+                    <li>Willian</li>
+                    <li>Matheus</li>
+                </ul>
             </div>
         </div>
     )
