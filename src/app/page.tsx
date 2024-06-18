9798df4df25d3c241fd1e034d8a5d088ed8e1122
matheuspinho <matheusluiz0303@outlook.com>
2024-06-18 09:56:44 -0300

    develop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import UltimasNoticias from "@/components/ultimas-noticias/ultimas-noticias";
 import Carousel from "@/components/ui/carousel/Carousel";
 import Institucional from "@/components/institucional/institucional";
 import ConvenienceAndSecurity from "@/app/pages/convenienceAndSecurity/page";
+import Carousel from "@/components/ui/carousel/Carousel";
 
 export default function Home() {
 
@@ -10,6 +11,29 @@ export default function Home() {
         <>
             <main>
                 <ConvenienceAndSecurity/>
+                <Carousel title="Dicas de economia e facilide pra te ajudar!" videos={[
+                    {
+                        title: "Iluminação Pública",
+                        description: "Este vídeo mostra informações sobre a iluminação pública.",
+                        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+                        thumbnailUrl: "https://picsum.photos/800/601"
+                    },
+                    {
+                        title: "Aprendendo sobre Energia Solar",
+                        thumbnailUrl: 'https://picsum.photos/800/603',
+                        description: "Um vídeo informativo sobre os benefícios da energia solar.",
+                        iframeUrl: "https://player.vimeo.com/video/929824958?h=3eb6a365ec",
+
+                    },
+                    {
+                        title: "Iluminação Pública",
+                        description: "Este vídeo mostra informações sobre a iluminação pública.",
+                        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+                        thumbnailUrl: "https://picsum.photos/800/604",
+
+                    },
+                ]} />
+
                 <UltimasNoticias/>
                 <Carousel title="Dicas de economia e facilide pra te ajudar!" videos={[
                     {
