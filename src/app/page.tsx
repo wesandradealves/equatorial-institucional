e4a81f83a099b144da7cb0a02c32d1c50487963a
matheuspinho <matheusluiz0303@outlook.com>
2024-06-18 10:15:16 -0300

    develop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,28 +34,6 @@ export default function Home() {
                 ]} />
 
                 <UltimasNoticias/>
-                <Carousel title="Dicas de economia e facilide pra te ajudar!" videos={[
-                    {
-                        title: "Iluminação Pública",
-                        description: "Este vídeo mostra informações sobre a iluminação pública.",
-                        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-                        thumbnailUrl: "https://picsum.photos/800/601"
-                    },
-                    {
-                        title: "Aprendendo sobre Energia Solar",
-                        thumbnailUrl: 'https://picsum.photos/800/603',
-                        description: "Um vídeo informativo sobre os benefícios da energia solar.",
-                        iframeUrl: "https://player.vimeo.com/video/929824958?h=3eb6a365ec",
-                        
-                    },
-                    {
-                        title: "Iluminação Pública",
-                        description: "Este vídeo mostra informações sobre a iluminação pública.",
-                        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-                        thumbnailUrl: "https://picsum.photos/800/604",
-                
-                    },
-                ]} />
                 <Institucional/>
                 <PerguntasFrequentes/>
             </main>
