2b4eff834ac22a57200ce308915f67e2b2cae401
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2024-06-24 14:59:54 -0300

    [MOD] Integração Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,28 @@ import Brand from './brand/brand';
 import Links from './links/links';
 import Contact from './contact/contact';
 
-const Footer = () => {
+export const dynamic = 'force-dynamic';
+// const getData = async (): Promise<T> => {
+//   const apiUrl = process.env.NEXT_PUBLIC_BASE_URL;
+//   const data = await fetch(`${apiUrl}/api/footer`, { method: 'GET', cache: 'no-cache' });
+
+//   return data.json();
+// }
+
+export default async function Footer() {
+
+  const apiUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const response = await fetch(`${apiUrl}/api/footer`, { method: 'GET', cache: 'no-cache' })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    // do something with data
+  })
+  .catch(rejected => {
+      console.log(rejected);
+  });
+  // const data = await getData();
+
   return (
     <footer>
       <Links/>
@@ -12,5 +33,3 @@ const Footer = () => {
     </footer>
   );
 };
-
-export default Footer;
