517877257ae698544f8e9f5d30026fe250b8c9b8
matheuspinho <matheusluiz0303@outlook.com>
2024-06-25 14:34:23 -0300

    task-4417 Integracao Header Ajuste

--- src/components/ui/header/status/status.tsx
+++ src/components/ui/header/status/status.tsx
@@ -1,19 +1,55 @@
+"use client"
 import styles from './status.module.scss';
+import {useEffect, useState} from "react";
+import {HttpService} from "@/services";
 
 /* eslint-disable-next-line */
 export interface StatusProps {}
 
+interface LocaleObject {
+    en:string,
+    pt_br:string
+}
+export interface TariffBand {
+    label:LocaleObject,
+    band:string
+}
+
+interface BuscarStatusRes {
+    data: Data
+}
+
+interface Data {
+    text:LocaleObject,
+    tariff_band:TariffBand,
+    searchbar:LocaleObject
+
+}
 export function Status(props: StatusProps) {
+    const http = new HttpService()
+    const [statusData,setStatusData] = useState<Data>()
+
+    const buscarStatus = async () => {
+        const result:BuscarStatusRes = await http.get('/api/header')
+        setStatusData(result.data)
+    }
+
+    useEffect(() => {
+        buscarStatus()
+    }, []);
   return (
     <div className={styles['container']}>
       <div className={styles['first']}>
-        <div className={styles['ellipse']}></div>
-        <span>Bandeira tarifária: verde</span>
+          {statusData && (<>
+              <div
+                  className={statusData && statusData?.tariff_band.band === 'Amarela' ? styles.ellipseYellow : styles.ellipse}></div>
+              <span>{statusData?.tariff_band.label.pt_br}: {statusData?.tariff_band.band}</span>
+          </>)}
       </div>
 
-      <div>
-        <span>Desligamento programado</span>
-      </div>
+        <div>
+            <span>{statusData?.text.pt_br}</span>
+        </div>
     </div>
   );
 }
