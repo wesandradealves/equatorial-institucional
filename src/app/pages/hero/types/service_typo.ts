6d39f31b38fb92ae07e8fd5d28da81dd07f8e12b
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2024-06-28 02:06:31 -0300

    [MOD] Integração Lista de Serviços

--- src/app/pages/hero/types/service_typo.ts
+++ src/app/pages/hero/types/service_typo.ts
@@ -1,10 +1,37 @@
-interface ServiceTypo {
+export interface ServiceTypo {
     id: string
     image: string
     title: string
     services: Number[]
 }
 
-// interface Service {
+export interface FieldImage {
+// "field_imagem": [
+//     {
+//         "target_id": 82,
+//         "alt": "Informar falta de luz",
+//         "title": "",
+//         "width": 32,
+//         "height": 32,
+//         "target_type": "file",
+//         "target_uuid": "344c2754-1cba-426a-a45a-e1f95f2ba7b6",
+//         "url": "https:\/\/b7ae-2804-214-8174-cd0b-a0fa-2e32-f4a4-e9cf.ngrok-free.app\/sites\/default\/files\/2024-06\/flash_off_0.png"
+//     }
+// ],
+}
 
-// }
+export interface fieldLink {
+//     "field_link": [
+//     {
+//         "value": "#"
+//     }
+// ],
+}
+
+export interface fieldTitulo {
+//  "field_titulo": [
+//     {
+//         "value": "Informar falta de luz"
+//     }
+// ]   
+}
