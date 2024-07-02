27f7314057bcd4b976dd56cbbb820e1fcd9b9e2a
matheuspinho <matheusluiz0303@outlook.com>
2024-07-02 14:33:31 -0300

    corrigido host images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
+'use client'
 import { Helmet } from 'react-helmet';
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/header";
 
 import "@/globals.scss";
-import Script from "next/script";
-import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const nonce = headers().get('x-nonce') as string|undefined
   return (
     <div id="wrap" className={`vh-100 d-flex flex-column ${inter.className}`}>
       <Helmet>
