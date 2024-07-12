2204812d9491458447fe5f5eccc4432a3c7ddaa1
wanderley-ferreira <wanderleyfa@gmail.com>
2024-07-12 09:03:11 -0300

    Atualizar componente NewsCard e UltimasNoticias

--- src/components/ultimas-noticias/ultimas-noticias.tsx
+++ src/components/ultimas-noticias/ultimas-noticias.tsx
@@ -1,20 +1,22 @@
 "use client";
-import styles from "./ultimas-noticias.module.scss";
+import { Button } from "@/assets/tsx/objects";
+import NewsCard from "@/components/ui/newsCard/NewsCard"; // Atualize com o caminho correto
 import { HttpService } from "@/services";
+import * as mdiIcons from "@mdi/js";
+import Icon from "@mdi/react";
 import { useEffect, useState } from "react";
 import { News, NewsTypo } from "./types/news_typo";
+import styles from "./ultimas-noticias.module.scss";
 
 export default function UltimasNoticias() {
   const http = new HttpService();
   const [news, setNews] = useState<News[]>([]);
-  // const [config, setConfig] = useState<ConfigTypo[]>([]);
 
-  const getUltimasNoticias = async() => {
-    // const config:ConfigTypo[] = await http.get('/api/config')
+  const getUltimasNoticias = async () => {
     const result: NewsTypo = await http.get("/api/noticias");
-    // setConfig(config);
     setNews(result.rows);
-  }  
+    console.log(result.rows);
+  };
 
   useEffect(() => {
     getUltimasNoticias();
@@ -28,29 +30,45 @@ export default function UltimasNoticias() {
             Fique por dentro<br></br> das últimas notícias
           </h4>
         </div>
-        {/* <div className={styles.action}>
-          <Button
-            label="Ver todas as notícias"
-            size="small"
-            variant="secondary"
-          />
-        </div> */}
+        <div className={styles.action}>
+          <Button>
+            Ver todas as notícias{" "}
+            <span className="icon-container">
+              <Icon path={mdiIcons.mdiArrowRight} size={1} className="icon" />
+            </span>
+          </Button>
+        </div>
         <div className={styles.news}>
-
-        {/* {news && news.map((item, index) => (
-            <NewsCard
-              key={index}
-              title={item.title}
-              body={item.summary}
-              mesano={item.date}
-              // imageUrl={`${props?.data?.basePath + props?.data?.logo}`}
-              imageUrl={'https://d792-191-165-222-70.ngrok-free.app/'+ item.image}
-            >
-              {item.category.map((tag, j) => (
-                <TagContent key={j} label={tag} />
-              ))}
-            </NewsCard>
-          ))} */}
+          {news &&
+            news.map((item, index) => (
+              <div key={index}>
+                {/* {item.title} -{" "}
+                <div dangerouslySetInnerHTML={{ __html: item.summary }}></div> -
+                {item.date} -{item.category} - {item.image} - */}
+                <NewsCard
+                  title={item.title}
+                  description={item.summary}
+                  image={item.image}
+                  link={item.image}
+                  date={item.date}
+                  category={item.category}
+                />
+                {/* <Image  */}
+                {/* <NewsCard title={item.title} body={item.body} mesano={item.date}> */}
+              </div>
+              // <NewsCard
+              //   key={index}
+              //   title={item.title}
+              //   body={item.summary}
+              //   mesano={item.date}
+              //   // imageUrl={`${props?.data?.basePath + props?.data?.logo}`}
+              //   imageUrl={'https://d792-191-165-222-70.ngrok-free.app/'+ item.image}
+              // >
+              //   {item.category.map((tag, j) => (
+              //     <TagContent key={j} label={tag} />
+              //   ))}
+              // </NewsCard>
+            ))}
         </div>
       </div>
     </>
