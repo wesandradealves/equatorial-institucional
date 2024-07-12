8c210d8b0e4702b8724fa835cb5c9afd2abe07e4
wanderley-ferreira <wanderleyfa@gmail.com>
2024-07-12 15:30:12 -0300

    Atualizar página inicial para corrigir blocos e renomear de "institucional" para "home"

--- src/app/home/(home)/page.tsx
+++ src/app/home/(home)/page.tsx
@@ -1,31 +1,34 @@
 "use client";
-import BlockShorts from '@/components/BlockShorts/BlockShorts'
-import { useContext, useEffect } from "react";
+import BlockShorts from "@/components/BlockShorts/BlockShorts";
 import ConfigProvider from "@/context/config";
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
-import PerguntasFrequentes from "@/components/perguntasFrequentes";
-import IniciativasEquatorial from "@/components/iniciativas-equatorial/iniciativas-equatorial";
-import Institucional from "@/components/institucional/institucional";
-import ConvenienceAndSecurity from '@/components/convenienceAndSecurity/convenienceAndSecurity';
-import Hero from '@/components/Hero/Hero';
-import UltimasNoticias from '@/components/ultimas-noticias/ultimas-noticias';
+// import PerguntasFrequentes from "@/components/perguntasFrequentes";
+// import IniciativasEquatorial from "@/components/iniciativas-equatorial/iniciativas-equatorial";
+// import Institucional from "@/components/institucional/institucional";
+// import ConvenienceAndSecurity from "@/components/convenienceAndSecurity/convenienceAndSecurity";
+import Hero from "@/components/Hero/Hero";
+import UltimasNoticias from "@/components/UltimasNoticias/UltimasNoticias";
 
 export default function Home() {
   const { config } = useContext<any>(ConfigProvider);
 
   return (
-    <>     
-      {config && <Helmet>
-        <title>{`${config?.site_name} - Página Inicial`}</title>
-      </Helmet>}     
+    <>
+      {config && (
+        <Helmet>
+          <title>{`${config?.site_name} - Página Inicial`}</title>
+        </Helmet>
+      )}
       <Hero />
+
       <BlockShorts />
       <UltimasNoticias />
       {/* <ConvenienceAndSecurity />
       <IniciativasEquatorial />
-      
+
       <Institucional />
       <PerguntasFrequentes />      */}
     </>
-  )
+  );
 }
