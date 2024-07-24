2e81477a58f3809639d471b5840d69ba91d08ab4
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-07-24 13:34:34 -0300

    Inserido validação de títuli no componente de intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -53,7 +53,7 @@ export default function Intro(props: any) {
         {props?.data?.field_layout_intro && props?.data?.field_layout_intro[0]?.value == 'alternative' && <WrapperMask className="--top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FBDB33" fillOpacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></WrapperMask>     }   
 
         <Container className="container d-flex align-items-start flex-wrap">
-          <BlockHead className="col-12 col-lg-3 pe-0" data={props?.data} />
+          {props?.data?.field_title && props?.data?.field_title?.value && <BlockHead className="col-12 col-lg-3 pe-0" data={props?.data} />}
           {props?.data.field_texto && <Text className="flex-fill" dangerouslySetInnerHTML={{__html: props?.data.field_texto[0]?.value}} />}
           
           {(props?.data.field_imagem && props?.data.field_imagem.length || (video && video?.thumbnail)) && <Thumbnail  
