ddbaa4921f8c140b3e4451887fd0f56d989355ba
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-08-01 23:34:19 -0300

    Ver mais noticias adicionado na single de noticias

--- src/app/home/noticias/[id]/page.tsx
+++ src/app/home/noticias/[id]/page.tsx
@@ -8,6 +8,9 @@ import DynamicComponent from "@/components/DynamicComponent/DynamicComponent";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Content } from "../../(home)/style";
 import { fetchData } from "@/app/layout";
+import BlockHead from "@/template-parts/BlockHead/BlockHead";
+import { Column, Columns, Container } from '@/components/UltimasNoticias/style';
+import NewsCard from "@/components/NewsCard/NewsCard";
 
 export const camelCase = (str:any) => {
   var splitStr = str.toLowerCase().split(' ');
@@ -18,17 +21,19 @@ export const camelCase = (str:any) => {
 }
 
 export default function Page(props: any) {
-  const router = useRouter();
   const { config } = useContext<any>(ConfigProvider);
   const http = new HttpService();
   const [data, setData] = useState<any>(null);
+  const [news, setNews] = useState<any>(null);
   const [content, setContent] = useState<any>(null);
-  const pathname = usePathname();
   
   useEffect(() => {
-    fetchData(`/api/noticia/${props?.params?.id}`).then((response: any) => {
-      if(response && response?.rows?.length) setData(response?.rows?.shift())
-    }).catch(console.error);     
+    Promise.all([`/api/noticia/${props?.params?.id}`, '/api/noticias?items_per_page=4'].map(function(url: any) {
+      return http.get(`${url}`);
+    })).then((response: any) => {
+      if(response[0] && response[0]?.rows?.length) setData(response[0]?.rows?.shift())
+      if(response[1] && response[1]?.rows?.length) setNews(response[1]?.rows)
+    });   
   }, [props]);    
 
   useEffect(() => {
@@ -39,22 +44,46 @@ export default function Page(props: any) {
         setContent(response)
       }).catch(console.error);   
     }
-
-    console.log(data)
   }, [data]);
 
   return <Content className="d-flex flex-column">
-    <>
       {config && <title>{`${config?.site_name} - ${data?.title}`}</title>}  
+
       {content && <>
         {content.map((component: any, index: Number) => (
           <DynamicComponent page={data} data={component} key={index} componentName={camelCase(component?.type[0]?.target_id.replaceAll("_"," ")).split(" ").join("")} />
         ))}
       </>}
-      {data?.body && <section>
-        <div className="container" dangerouslySetInnerHTML={{ __html: data?.body }} />
-      </section>}
-    </>
 
+      {data?.body && <section>
+        <Container className="container" dangerouslySetInnerHTML={{ __html: data?.body }} />
+      </section>}
+
+      {news && <section>
+        <Container className="container">
+          <BlockHead className="col-12" data={{
+            title: "<strong>Ver mais notícias</strong>"
+          }} />
+          <Columns className="d-flex mt-5">
+            <Column className='col-12 flex-fill d-flex flex-wrap align-items-stretch'>
+              {news.map((row: any, index: any) => (
+                <>
+                  <NewsCard
+                    nid={row.nid}
+                    key={index}
+                    index={index}
+                    title={row.title}
+                    description={row.summary}
+                    image={config?.basePath + row.image}
+                    link={row.image}
+                    date={row.date}
+                    category={row.category}
+                    className="col-12 col-md-6 col-lg-3"
+                  />              
+                </>
+            ))}</Column>    
+          </Columns>     
+        </Container>
+      </section>}
   </Content>;
 }
