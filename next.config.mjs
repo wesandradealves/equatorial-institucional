321c06c8c92876c2734040751753ef4d51af0534
Wanderley ferreira de Albuquerque (Sysmanager) <T46586@eqtlcontratada.com.br>
2024-08-28 13:27:51 -0300

    Atualizar variáveis de ambiente no arquivo .env.example

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 import path from "node:path";
 
 const cspHeader = `
-    default-src 'self' http://drupal-institucional-drupal-dev.apps.ocpdrupal.equatorial.corp;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' http://drupal-institucional-drupal-dev.apps.ocpdrupal.equatorial.corp;
-    style-src 'self' 'unsafe-inline' http://drupal-institucional-drupal-dev.apps.ocpdrupal.equatorial.corp https://fonts.googleapis.com/;
-    img-src 'self' blob: data: http://drupal-institucional-drupal-dev.apps.ocpdrupal.equatorial.corp https://picsum.photos;
+    default-src 'self' ;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' ;
+    style-src 'self' 'unsafe-inline'  https://fonts.googleapis.com/;
+    img-src 'self' blob: data:  https://picsum.photos;
     font-src 'self' https://fonts.gstatic.com;
     object-src 'none';
-    base-uri 'self' http://drupal-institucional-drupal-dev.apps.ocpdrupal.equatorial.corp ;
+    base-uri 'self'  ;
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
-`
+`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -24,24 +24,24 @@ const nextConfig = {
       {
         hostname: "**",
       },
-    ],    
+    ],
   },
   compiler: {
-    styledComponents: true
-  },  
+    styledComponents: true,
+  },
   swcMinify: true,
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
           },
         ],
       },
-    ]
+    ];
   },
 };
 
