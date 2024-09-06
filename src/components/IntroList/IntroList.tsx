ebd2a92264b6784dd1fe86395898c76fd6b132a4
matheuspinho <matheusluiz0303@outlook.com>
2024-09-06 15:13:14 -0300

    [BUGFIX]-5501

--- src/components/IntroList/IntroList.tsx
+++ src/components/IntroList/IntroList.tsx
@@ -45,8 +45,8 @@ export default function IntroList(props: any) {
             </Column>}
           </Container>
         </Container>
-        <Mask className="--bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FBDB33" fillOpacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></Mask>        
-        <Mask className="--bottom --backdrop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FBDB33" fillOpacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></Mask>        
+        <Mask className="--bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FBDB33" fillOpacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></Mask>
+        <Mask className="--bottom --backdrop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" height="600"><path fill="#FBDB33" fillOpacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,202.7C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></Mask>
       </Content>}    
     </>
   );
