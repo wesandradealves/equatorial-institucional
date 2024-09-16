8810f1f77c88262aa3129055767f73d7773e2a44
Wanderley ferreira de Albuquerque (Sysmanager) <T46586@eqtlcontratada.com.br>
2024-09-16 17:29:08 -0300

    adicionando teste para saber em qual estado ficara o dropdow da pagina inicial

--- src/components/LocationSelector/LocationSelector.tsx
+++ src/components/LocationSelector/LocationSelector.tsx
@@ -1,31 +1,45 @@
 "use client";
 
-import { Container, Label, Select, SelectField, Option } from "./style";
-import { useContext, useEffect, useState } from "react";
 import { NavTypo } from "@/types/enums";
 import { fetchData } from "@/utils";
+import { useEffect, useState } from "react";
+import { Container, Label, Option, Select, SelectField } from "./style";
 
 export default function LocationSelector(props: any) {
   const [navigation, setData] = useState<NavTypo[]>([]);
-  
+
   useEffect(() => {
-    fetchData('/api/menu_items/location').then((response: any) => {
+    fetchData("/api/menu_items/location").then((response: any) => {
       setData(response);
-    })   
+    });
   }, []);
 
-  return (navigation && <Container className="location-selection d-flex align-items-center">
-    <Label>Você está em</Label>
-    <Select className="me-auto">
-      <SelectField onChange={(e: any) => {
-        window.location.href = e.target.value
-      }}>
-        {navigation.map(function(row:any, i:number){
-          return (
-            <Option value={row?.absolute} key={i}>{row?.title}</Option>
-          );
-        })}        
-      </SelectField>
-    </Select>
-  </Container>);
+  return (
+    navigation && (
+      <Container className="location-selection d-flex align-items-center">
+        <Label>Você está em</Label>
+        <Select className="me-auto">
+          <SelectField
+            onChange={(e: any) => {
+              window.location.href = e.target.value;
+            }}
+          >
+            {navigation.map(function (row: any, i: number) {
+              return (
+                <Option
+                  value={row?.absolute}
+                  key={i}
+                  selected={
+                    row?.title === process.env.NEXT_PUBLIC_ESTADO ? true : false
+                  }
+                >
+                  {row?.title}
+                </Option>
+              );
+            })}
+          </SelectField>
+        </Select>
+      </Container>
+    )
+  );
 }
